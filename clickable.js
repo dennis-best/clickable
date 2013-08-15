501b369e1990b3a54b03404a679edad160e6dfdb
dennis-best <dennis.best@me.com>
2013-08-14 18:00:21 -0700

    Adds logic to toggle the clicked class

--- clickable.js
+++ clickable.js
@@ -1,8 +1,13 @@
 (function($){
 		 $.fn.clickable=function(update_id){
 				 this.click( function(event) { 
-					 $(update_id).removeClass("clicked"); 
-					 $(event.target).closest(update_id).addClass("clicked"); 
+					 if ($(event.target).closest(update_id).hasClass("clicked")) {
+						 	$(update_id).removeClass("clicked"); 
+					 	}
+						else {
+							$(update_id).removeClass("clicked"); 
+							$(event.target).closest(update_id).addClass("clicked"); 
+						}
 				});
 		 }
 })(jQuery);
