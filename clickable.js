666679ac337a0cd0176cfe3e72efcecac897bf73
dennis-best <dennis.best@me.com>
2013-08-14 19:44:09 -0700

    Limits click to clickable element regardless of context

--- clickable.js
+++ clickable.js
@@ -1,13 +1,15 @@
 (function($){
-		 $.fn.clickable=function(update_id){
-				 this.click( function(event) { 
-					 if ($(event.target).closest(update_id).hasClass("clicked")) {
-						 	$(update_id).removeClass("clicked"); 
-					 	}
-						else {
-							$(update_id).removeClass("clicked"); 
-							$(event.target).closest(update_id).addClass("clicked"); 
+	 $.fn.clickable=function(update_id){
+		 this.click( function(event) { 
+			 if ($(event.target).is(update_id)) {
+				 if ($(event.target).closest(update_id).hasClass("clicked")) {
+						$(update_id).removeClass("clicked"); 
 						}
-				});
-		 }
+					else {
+						$(update_id).removeClass("clicked"); 
+						$(event.target).closest(update_id).addClass("clicked"); 
+					}
+			 }
+		});
+	 }
 })(jQuery);
